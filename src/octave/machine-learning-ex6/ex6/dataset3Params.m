c878e01d028218b708657d110d0ba619fd467ddd
Dai Nguyen-Van <dainv1989@hotmail.com>
2019-10-16 22:26:13 +0700

    solution of parameters (C, sigma) for dataset 3

--- src/octave/machine-learning-ex6/ex6/dataset3Params.m
+++ src/octave/machine-learning-ex6/ex6/dataset3Params.m
@@ -27,11 +27,34 @@ C_vec = [0.01 0.03 0.1 0.3 1 3 10 30]';
 sigma_vec = C_vec;
 
 m = length(C_vec);
-error = zeros(m, 1);
+error = zeros(m, m);
 
 for i = 1:m
+    C = C_vec(i);
+    for j = 1:m
+        sigma = sigma_vec(j);
+        # train model for every pair of C & sigma
+        model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
+        predictions = svmPredict(model, Xval);
+        
+        # calculate error of each prediction set
+        error(i, j) = mean(double(predictions ~= yval));
+    end
+end
 
+min_error = error(1, 1);
+C = C_vec(1);
+sigma = sigma_vec(1);
 
+# get result of least error pair of C & sigma
+for i = 1:m
+    for j = 1:m
+        if (error(i, j) < min_error)
+            C = C_vec(i);
+            sigma = sigma_vec(j);
+            min_error = error(i, j);
+        end
+    end
 end
 % =========================================================================
 
