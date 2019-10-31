b95ad1ea0e258f9b0fe68c87f4ecddd7133c7913
Dai Nguyen-Van <dainv1989@hotmail.com>
2019-10-31 22:23:10 +0700

    simple python plot with 2 diagrams

--- data-visualize/simple_plot.py
+++ data-visualize/simple_plot.py
@@ -10,14 +10,23 @@ y = []
 for i in range(len(x)):
     y.append(math.sin(x[i]))
 
-fig, ax = plt.subplots()
-print(type(ax))
-print(type(fig))
-ax.plot(x, y)
-ax.set_xlabel('X data')
-ax.set_ylabel('Y data')
-ax.set_title('y = sin(x) diagram')
-plt.show()
+# shorten version with numpy library
+# y = np.sin(x)
+
+# fig, ax = plt.subplots()
+# print(type(ax))
+# print(type(fig))
+# ax.plot(x, y)
+# ax.set_xlabel('X data')
+# ax.set_ylabel('Y data')
+# ax.set_title('y = sin(x) diagram')
+
+plt.subplot(2, 1, 1)
+plt.plot(x, y)
+plt.title('y = sin(x) diagram')
+plt.xlabel('X data')
+plt.ylabel('Y data')
+# plt.show()
 
 a = np.linspace(-math.pi, math.pi, 50, dtype=float)
 # print(a)
@@ -25,3 +34,13 @@ a = np.linspace(-math.pi, math.pi, 50, dtype=float)
 b = []
 for i in range(len(a)):
     b.append(math.cos(a[i]))
+
+# shorten version with numpy library
+b = np.cos(a)
+
+plt.subplot(2, 1, 2)
+plt.plot(a, b)
+plt.title('a = sin(b)')
+plt.xlabel('A data')
+plt.ylabel('B data')
+plt.show()
