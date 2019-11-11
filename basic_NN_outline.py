fdbb9b115edcf08861e6980c790d04f4a7df417e
Dai Nguyen-Van <dainv1989@hotmail.com>
2019-11-11 17:37:03 +0700

    basic neural network implementation outline

---
+++ basic_NN_outline.py
@@ -0,0 +1,19 @@
+def initialize_parameters_deep(layers_dims):
+    ...
+    return parameters 
+
+def L_model_forward(X, parameters):
+    ...
+    return AL, caches
+
+def compute_cost(AL, Y):
+    ...
+    return cost
+
+def L_model_backward(AL, Y, caches):
+    ...
+    return grads
+
+def update_parameters(parameters, grads, learning_rate):
+    ...
+    return parameters
