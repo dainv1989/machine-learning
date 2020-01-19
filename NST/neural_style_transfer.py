c99ba43b882ae1c72265e0397bb2a8bc4dd4665d
Dai Nguyen-Van <dainv1989@hotmail.com>
2020-01-19 22:40:01 +0700

    add style images, update support functions

--- NST/neural_style_transfer.py
+++ NST/neural_style_transfer.py
@@ -3,12 +3,14 @@
 # References:
 # [1] https://www.virtosuart.com/blog/abstract-art
 # [2] https://www.cs.toronto.edu/~frossard/post/vgg16/
-# [3] https://www.tensorflow.org/tutorials/generative/style_transfer
+# [3] https://towardsdatascience.com/light-on-math-machine-learning-intuitive-guide-to-neural-style-transfer-ef88e46697ee
 #
 
 import tensorflow as tf
 import numpy as np
 import scipy.io
+from collections import OrderedDict
+import re               # regular expression
 
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
@@ -50,7 +52,7 @@ def imshow(image1, image2 = None):
 #imshow(content_image, style_image)
 
 # implementation outline
-def load_vgg_weights(model_weights_file):
+def load_weights(model_weights_file, end_layer = 'all'):
     """
     Load pretrained VGG weights from file. Apply to load vgg weights from ref [2]
 
@@ -60,22 +62,63 @@ def load_vgg_weights(model_weights_file):
     Return:
     vgg_weights         -- weights data in dictionary format
     """
+    layers = OrderedDict()
     weights = np.load(model_weights_file)
     vgg_weights = sorted(weights.items())
 
+    layer_count = 0
     for i, (k, w) in enumerate(vgg_weights):
-        print("{} : {} - {} - {}".format(i, k, w.shape, w.dtype))
+        if (end_layer != 'all') and (layer_count >= int(end_layer)):
+            break
+
+        # k = conv1_2_W --> k[:-2] = conv1_2
+        if k[:-2] not in layers:
+            layers[k[:-2]] = {}
+
+        if re.search(r'conv\d+_\d+_W', k) is not None or re.search(r'fc\d+_W', k) is not None:
+            layers[k[:-2]]['weights'] = w
+        if re.search(r'conv\d+_\d+_b', k) is not None or re.search(r'fc\d+_b', k) is not None:
+            layers[k[:-2]]['bias'] = w
+
+        print("loaded layer {}: {} - {}".format(layer_count, k, w.shape))
+        if (k[:-2] in layers) and ('bias' in layers[k[:-2]]):
+            layer_count += 1
     return vgg_weights
 
 # DEBUG
-#vgg16_weights = "vgg16_weights.npz"
-#load_vgg_weights(vgg16_weights)
+vgg16_weights = "vgg16_weights.npz"
+load_weights(vgg16_weights, 7)
 
-def load_mat_file(mat_model_file):
-    model = scipy.io.loadmat(mat_model_file)
-    for i, (k, w) in enumerate(model.items()):
-        print("{}".format(k))
-    return model
+def vgg_nst(input_image, layer_ids, pool_ids, vgg_layers):
+    outputs = OrderedDict()
+    out = input_image
+
+    for l_id in layer_ids:
+        with tf.variable_scope(l_id, reuse = tf.AUTO_REUSE):
+            print("Computing output at layer {}".format(l_id))
+            w, b = tf.get_variable('weights'), tf.get_variable('bias')
+            out = tf.nn.conv2d(filter=w, input=out, strides=[1, 1, 1, 1], padding='SAME')
+            out = tf.nn.relu(tf.nn.bias_add(value=out, bias=b))
+            outputs["conv" + l_id] = out
+
+        if l_id in pool_ids:
+            out = tf.nn.max_pool(input=out, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+            outputs["pool" + l_id] = out
+    return outputs
+
+def initalize_input(input_shape):
+    content = tf.placeholder(name='content', shape=input_shape, dtype=tf.float32)
+    style = tf.placeholder(name='style', shape=input_shape, dtype=tf.float32)
+    generated = tf.placeholder(name='generated', shape=input_shape, dtype=tf.float32, 
+                               initializer=tf.random_normal_initializer, trainable=True)
+    return {'content': content, 'style': style, 'generated': generated}
+
+def define_tf_weights(vgg_layers):
+    for k, wb in vgg_layers.items():
+        w, b = wb['weights'], wb['bias']
+        with tf.variable_scope(k):
+            tf.get_variable(name='weights', initializer=tf.constant(w, dtype=tf.float32), trainable=False)
+            tf.get_variable(name='bias', initializer=tf.constant(b, dtype=tf.float32), trainable=False)
 
 # DEBUG
 #vgg19_model_file = 'imagenet-vgg-verydeep-19.mat'
