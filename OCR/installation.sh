302f6c99392484d919e88a74298ae569ceb77367
Dai Nguyen-Van <dainv1989@hotmail.com>
2020-03-20 23:05:16 +0700

    update credit card number reading with tesseract

--- OCR/installation.sh
+++ OCR/installation.sh
@@ -5,3 +5,5 @@ pip3 install opencv-python
 pip3 install --upgrade imutils
 # execute
 # python3 tesseract-test.py
+# python3 add-noise.py -i images/image.jpg -t gauss --ratio 0.7
+# python3 ocr-credit-card.py -i images/credit_card_01.png -r images/ocr_a_reference.png
--- OCR/ocr-credit-card.py
+++ OCR/ocr-credit-card.py
@@ -27,4 +27,22 @@ ref = cv2.threshold(ref, 10, 255, cv2.THRESH_BINARY_INV)[1]
 #cv2.imshow("reference", ref)
 #cv2.waitKey(0)
 
+refCnts = cv2.findContours(ref.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#print(type(refCnts)) # tuple
+#print(refCnts)
+refCnts = imutils.grab_contours(refCnts)
+refCnts = contours.sort_contours(refCnts, method="left-to-right")[0]
+digits = {}
+
+for (i, c) in enumerate(refCnts):
+    # compute bounding box for the digit extract and resize it to fixed size
+    (x, y, w, h) = cv2.boundingRect(c)
+    roi = ref[y:y + h, x:x + w]
+    roi = cv2.resize(roi, (57, 88))
+    # update digits dict, mapping to ROI
+    digits[i] = roi
+#end for (i, c)
+
+#cv2.imshow("8", digits[8])
+#cv2.waitKey(0)
 
